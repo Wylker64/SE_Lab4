864aec88d618f9370d81f75351e154a202e0163f
Wylker <wylker@qq.com>
2023-05-27 14:43:48 +0800

    Update FlowController.java

--- backend/src/main/java/com/segroup2023/lab/controller/flow/FlowController.java
+++ backend/src/main/java/com/segroup2023/lab/controller/flow/FlowController.java
@@ -19,8 +19,8 @@ public class FlowController {
     private FlowService flowService;
 
     @GetMapping("/get")
-    public ResponseEntity<List<FlowEntity>> getFlow(@RequestParam("account_id") Long accountId, @RequestParam("page_num") int pageNum, @RequestParam("select") int select) {
-        List<FlowEntity> entities = flowService.getFlow(accountId, select);
+    public ResponseEntity<List<FlowEntity>> getFlow(@RequestParam("account_id") Long accountId, @RequestParam("select") int select) {
+        List<FlowEntity> entities = FlowService.getFlow(accountId, select);
         return ResponseEntity.ok(entities);
     }
 
