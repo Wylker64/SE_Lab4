ba3c2b621580217df7d18a611e4633d7f4bf7d98
Wylker <wylker@qq.com>
2023-05-27 19:16:18 +0800

    [refactor]Moved IllegalArgumentException to GlobalExceptionHandler.
    return: FORBIDDEN 403

--- backend/src/main/java/com/segroup2023/lab/exception/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/segroup2023/lab/exception/handler/GlobalExceptionHandler.java
@@ -6,11 +6,15 @@ import com.segroup2023.lab.exception.type.LoginFailureException;
 import com.segroup2023.lab.exception.type.PermissionDeniedException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @ControllerAdvice
@@ -44,6 +48,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(PermissionDeniedException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public String permissionDenied(PermissionDeniedException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String handleIllegalArgument(IllegalArgumentException e){
         return e.getMessage();
     }
 }
