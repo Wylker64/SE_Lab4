a07b3011481897142c82ccd811b6e88b1f09a99f
Octopus314 <mikezhang_11@hotmail.com>
2023-05-28 15:01:48 +0800

    [new] product info

--- backend/src/main/java/com/segroup2023/lab/controller/product/ProductQueryController.java
+++ backend/src/main/java/com/segroup2023/lab/controller/product/ProductQueryController.java
@@ -12,26 +12,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/product/{id}")
 public class ProductQueryController {
-    private static class SingleRequest {
-        @Valid
-        @NotNull
-        public UserAuthorization.Request authorize;
-    }
 
-    private static class SingleResponse {
-        public Product product;
-    }
-
-    @PostMapping("")
-    public SingleResponse product(@PathVariable(name = "id") @NotNull Long productId, @RequestBody @Valid @NotNull SingleRequest request)
-        throws LoginFailureException {
-        User user = UserAuthorization.authorize(request.authorize);
-        Product product = ProductService.getProduct(productId);
-        if(user.getIdentity().equals(User.Identity.USER) && !product.getApproved())
-            return null;
-        SingleResponse response = new SingleResponse();
-        response.product = product;
-        return response;
+    @GetMapping("")
+    public Product product(@PathVariable(name = "id") @NotNull Long productId) {
+        return ProductService.getProduct(productId);
     }
 
 }
